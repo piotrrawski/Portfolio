6d1e641a8f6446364f4fe0b891d5a808032934ca
piotrrawski <piotr.rawski@gmail.com>
2018-01-06 12:27:10 +0100

    changing catalogue structure, creating webpack

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+//FOR REACT
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: ["whatwg-fetch","./src/jsx/App.jsx","./src/scss/style.scss"],
+    output: { filename: "./dist/js/out.js" },
+    devServer: {
+        inline: true,
+        contentBase: './',
+        port: 3001
+    },
+    watch: true,
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: { presets: ['es2015', 'stage-2', 'react'] }
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader?url=false', 'sass-loader']
+                })
+
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {}
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('./dist/css/style.css')
+
+    ]
+};
