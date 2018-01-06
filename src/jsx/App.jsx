5a322b340da28f0951c00b77b2508f1ff3c8bcf9
piotrrawski <piotr.rawski@gmail.com>
2018-01-06 18:28:22 +0100

    creating grid and header

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -1,1 +1,65 @@
 console.log('hello');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router,
+    Route,
+    Link,
+    IndexLink,
+    IndexRoute,
+    hashHistory
+} from 'react-router';
+
+
+
+class ViewOne extends React.Component {
+    render () {
+        return (
+            <div className="background">
+                <Header />
+            </div>
+        )
+    }
+}
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header>
+                <section className="row">
+                    <div className="col-6 logo">Piotr Rawski</div>
+                    <div className="col-6 menu"><Menu /></div>
+                </section>
+            </header>
+        )
+    }
+}
+
+class Menu extends React.Component {
+    render () {
+        return (
+            <ul>
+                <li><a href="/">About</a></li>
+                <li><a href="/">Skills</a></li>
+                <li><a href="/">My work</a></li>
+                <li><a href="/">Contact</a></li>
+            </ul>
+        )
+    }
+
+}
+
+class App extends React.Component {
+    render () {
+        return (
+          <ViewOne/>
+        )
+}
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    ReactDOM.render(
+        <App/>,
+        document.getElementById('app')
+    );
+});
